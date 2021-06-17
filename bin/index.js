1c660d595e2ce8ad26f67560768db6130d3299f8
yangbin <1792433136@qq.com>
2021-06-17 11:27:54 +0800

    update commander

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,16 @@
 #!/usr/bin/env node
+const { program } = require('commander')
 
-console.log('----init----')
+program.option('-c, --cheese [type]', 'Add cheese with optional type')
+
+program.parse(process.argv)
+
+const options = program.opts()
+
+if (options.cheese === undefined) {
+  console.log('no cheese')
+} else if (options.cheese === true) {
+  console.log('add cheese')
+} else {
+  console.log(`add cheese type ${options.cheese}`)
+}
