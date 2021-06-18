d43e1de520decf767e503d4430d5c993aeaa5ca3
littlebearbbb <1792433136@qq.com>
2021-06-18 11:58:12 +0800

    update package html

--- bin/index.js
+++ bin/index.js
@@ -1,16 +1,36 @@
 #!/usr/bin/env node
 const { program } = require('commander')
+const fs = require('fs')
+const cheerio = require('cheerio')
 
-program.option('-c, --cheese [type]', 'Add cheese with optional type')
+program.option('-h, --html [htmlUrl]', 'add manifest & sw', './index.html')
 
-program.parse(process.argv)
+program.parse()
 
 const options = program.opts()
 
-if (options.cheese === undefined) {
-  console.log('no cheese')
-} else if (options.cheese === true) {
-  console.log('add cheese')
-} else {
-  console.log(`add cheese type ${options.cheese}`)
-}
+const data = fs.readFileSync(options.html)
+
+//加载HTML字符串
+const $ = cheerio.load(data.toString())
+
+$('title').before('<link rel="manifest" href="./manifest.json" />')
+$('body').append(`
+ <script>
+   if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.getRegistrations().then(regs => {
+      for (let reg of regs) {
+        reg.unregister()
+      }
+      navigator.serviceWorker.register('./sw.js')
+    })
+   }
+ </script>
+`)
+
+//HTML文本内容
+const html = $.html()
+
+console.log('---html 文件内容---')
+console.log(html)
+
